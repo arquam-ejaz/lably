57793f0f73ca906e42175ffa4d161eda53c348dd
arquamejaz <arquam.ejaz@gmail.com>
2021-04-30 01:01:01 +0530

    Added entities Admin, Customer, and Tests

---
+++ src/main/java/com/xfactor/lably/entity/Customer.java
@@ -0,0 +1,41 @@
+package com.xfactor.lably.entity;
+
+public class Customer {
+    private String name;
+    private String address;
+    private String age;
+    private String gender;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAge() {
+        return this.age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
---
+++ src/main/java/com/xfactor/lably/entity/Tests.java
@@ -0,0 +1,32 @@
+package com.xfactor.lably.entity;
+
+public class Tests {
+    private String name;
+    private String description;
+    private double price;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
